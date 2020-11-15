f0c75abdc27409057be4fc957aaca18762427e56
sun-5 <987887117@qq.com>
2020-11-15 14:57:19 +0800

    20201115

--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+//解决重复点击路由报错问题
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
